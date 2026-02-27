5c0b7773497c962267fe64f542cd8b583410ef6d
zhoutaipiing <1164021461@qq.com>
2026-02-27 15:15:14 +0800

    add darp plugin

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import './main.css'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import VueDraggableResizable from 'vue-draggable-resizable'
 
-
-createApp(App).use(ElementPlus, { size: 'small', zIndex: 3000 }).mount('#app')
+// 注册全局组件
+const app = createApp(App)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+app.component("vue-draggable-resizable", VueDraggableResizable)
+app.mount('#app')
 
